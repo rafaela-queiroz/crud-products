3e62d12d677102a048b0fed5c53899010447e80b
Rafaela Queiroz <rqueiroz2108@gmail.com>
2020-12-08 21:18:35 -0300

    :tada: First commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
