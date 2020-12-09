135e90929ba23564938bbe86e03562ec262a4767
Rafaela Queiroz <rqueiroz2108@gmail.com>
2020-12-08 23:21:25 -0300

    :sparkles: Add routes and directive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -9,9 +10,20 @@ import { AppComponent } from "./app.component";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { HeaderComponent } from "./components/template/header/header.component";
 import { NavComponent } from "./components/template/nav/nav.component";
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,6 +31,7 @@ import { NavComponent } from "./components/template/nav/nav.component";
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
